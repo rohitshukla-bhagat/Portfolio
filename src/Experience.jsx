0b345aa5802adf91156eef93de0a4d640232631d
Rohit Shukla <aiedutube@gmail.com>
2025-08-26 19:56:34 +0530

    add experince

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -15,6 +15,37 @@ export default function ExperiencePage() {
             <div className="experienceBox1">
               <div className="expe1">
                 <div className="expeHead">
+                  <img src="./bluestock.webp" alt="" />
+                  <div>
+                    <p className="expeRole">SDE Intern</p>
+                    <p className="expeComName">Bluestock Fintech</p>
+                    <p className="expeDate">1 June 2025 - 31 July 2025</p>
+                  </div>
+                </div>
+                <div className="expeDiscription">
+                  <p>
+                    As a Software Developer Engineer Intern in Bluestock Fintech i was working on projects like IPO Web Application and Machine Learning Company Analysis , with the MERN stacks like NodeJS , MongoDB , ExpressJS , ReactJS , JavaScript , CSS , HTML , etc. These projects is help to build my strong command on mentioned tech stacks with the bonus knowledge of Stock Martket and how the ML logic works at the backend.
+                  </p>
+                </div>
+                <div className="expeSkills">
+                  <p>Skills:</p>
+                  <div>
+                    <ul>
+                      <li>Nodejs</li>
+                      <li>ExpressJS</li>
+                      <li>MongoDB</li>
+                      <li>ReactJS</li>
+                      <li>JavaScript</li>
+                      <li>CSS</li>
+                      <li>HTML</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="experienceBox2">
+              <div className="expe1">
+                <div className="expeHead">
                   <img src="./softapper.jpeg" alt="" />
                   <div>
                     <p className="expeRole">Frontend Developer Intern</p>
@@ -27,10 +58,10 @@ export default function ExperiencePage() {
                     I developed the two fontend project using the basic frontend
                     core HTML , CSS and JavaScript during this online
                     internship. The first project is "To-Do List" Application
-                    where you can Add , Edit , Mark as done and Delete your
-                    task easily and the second project is "Basic Calculator"
-                    where you can easily perform basic mathematical operations
-                    like Addition , Substraction , Multiplication , Division and
+                    where you can Add , Edit , Mark as done and Delete your task
+                    easily and the second project is "Basic Calculator" where
+                    you can easily perform basic mathematical operations like
+                    Addition , Substraction , Multiplication , Division and
                     Modulus.
                   </p>
                 </div>
@@ -46,7 +77,6 @@ export default function ExperiencePage() {
                 </div>
               </div>
             </div>
-            <div className="experienceBox2"></div>
           </div>
         </div>
       </div>
